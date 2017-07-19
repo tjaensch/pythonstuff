67a95360c73a927681018842c91c5074a4ecf6b4
Thomas Jaensch <thomas.jaensch@noaa.gov>
2017-07-19 18:22:30 +0000

    Some cosmetic edits

--- src/ghcn/test.py
+++ src/ghcn/test.py
@@ -1,14 +1,12 @@
 import numpy as np
 import netCDF4
 
-# this load the file into a Nx3 array (three columns)
-data = np.genfromtxt('1763.csv', dtype=str, delimiter=',')
+# Load source CSV file into variable
+data = np.genfromtxt('/nodc/projects/satdata/Granule_OneStop/GHCN/source_csv_by_year/1763.csv', dtype=str, delimiter=',')
 
-
-# create a netcdf Data object
-
+# Create a netcdf Data object
 with netCDF4.Dataset('TEST_file.nc', mode="w", format='NETCDF4') as ds:
-    # some file-level meta-data attributes:
+    # File-level metadata attributes:
     ds.Conventions = "CF-1.6" # if you comply with the convension -- which you should!
     ds.title = 'A succinct description of what is in the dataset.'
     ds.institution = 'Specifies where the original data was produced.'
@@ -17,15 +15,15 @@ with netCDF4.Dataset('TEST_file.nc', mode="w", format='NETCDF4') as ds:
     ds.references = 'References'
     ds.comment = 'whatever comment you may want to add'
 
-    # defining the dimensions of your arrays:
+    # Defining array dimensions
     level = ds.createDimension('level', data.shape[0])
 
     # variables for the columns -- you should use real names
     for i in range(data.shape[1]):
         var = ds.createVariable('var%i'%i,data.dtype, ('level',))
         var[:] = data[:,i]
-        ## adds some attributes
-        var.units = 'the_proper_unit_string'
-        var.long_name = 'a nice long name that describes the data'
-        var.standard_name = 'a_CF_standard_name'
+        # Add attributes
+        # var.units = 'the_proper_unit_string'
+        # var.long_name = 'long name that describes the data'
+        # var.standard_name = 'CF_standard_name'
     print ds
