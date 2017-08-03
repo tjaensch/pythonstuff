774c6f96f515b6c972b14fca115558150cdfe579
Thomas Jaensch <thomas.jaensch@noaa.gov>
2017-08-03 17:12:09 +0000

    Finished changes to numberedList data matrix

--- src/ghcn/many_vars_data_arrays_solution/ghcn.py
+++ src/ghcn/many_vars_data_arrays_solution/ghcn.py
@@ -21,14 +21,14 @@ class GHCN:
     '''thomas.jaensch@noaa.gov'''
 
     def __init__(self):
-        # Lists and dictionaries with information from https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt to be used in netCDF variables derived with def get_stationInfo
+        # Lists and dictionaries with information from ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt to be used in netCDF variables derived with def get_stationInfo
         self.stationIds = []
         self.latDict = {}
         self.lonDict = {}
         self.stationLongNameDict = {}
 
     def get_stationInfo(self):
-        data = urllib2.urlopen("https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt")
+        data = urllib2.urlopen("ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/ghcnd-stations.txt")
         for line in data:
             if not line:
                 break
@@ -43,7 +43,7 @@ class GHCN:
 
     def download_dly_file(self, fileId):
         try:
-            url = 'https://www1.ncdc.noaa.gov/pub/data/ghcn/daily/all/%s.dly' %fileId
+            url = 'ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/all/%s.dly' %fileId
             urllib.urlretrieve(url, 'dly_data_as_txt/' + fileId + '.txt')
         except:
             logging.exception(fileId + ": ")
@@ -58,75 +58,44 @@ class GHCN:
             YEAR = []
             MONTH = []
             ELEMENT = []
-            # 4 lists for each day of the month 1-31
-            VALUE1 = []; MFLAG1 = []; QFLAG1 = []; SFLAG1 = [];
-            VALUE2 = []; MFLAG2 = []; QFLAG2 = []; SFLAG2 = [];
-            VALUE3 = []; MFLAG3 = []; QFLAG3 = []; SFLAG3 = [];
-            VALUE4 = []; MFLAG4 = []; QFLAG4 = []; SFLAG4 = [];
-            VALUE5 = []; MFLAG5 = []; QFLAG5 = []; SFLAG5 = [];
-            VALUE6 = []; MFLAG6 = []; QFLAG6 = []; SFLAG6 = [];
-            VALUE7 = []; MFLAG7 = []; QFLAG7 = []; SFLAG7 = [];
-            VALUE8 = []; MFLAG8 = []; QFLAG8 = []; SFLAG8 = [];
-            VALUE9 = []; MFLAG9 = []; QFLAG9 = []; SFLAG9 = [];
-            VALUE10 = []; MFLAG10 = []; QFLAG10 = []; SFLAG10 = [];
-            VALUE11 = []; MFLAG11 = []; QFLAG11 = []; SFLAG11 = [];
-            VALUE12 = []; MFLAG12 = []; QFLAG12 = []; SFLAG12 = [];
-            VALUE13 = []; MFLAG13 = []; QFLAG13 = []; SFLAG13 = [];
-            VALUE14 = []; MFLAG14 = []; QFLAG14 = []; SFLAG14 = [];
-            VALUE15 = []; MFLAG15 = []; QFLAG15 = []; SFLAG15 = [];
-            VALUE16 = []; MFLAG16 = []; QFLAG16 = []; SFLAG16 = [];
-            VALUE17 = []; MFLAG17 = []; QFLAG17 = []; SFLAG17 = [];
-            VALUE18 = []; MFLAG18 = []; QFLAG18 = []; SFLAG18 = [];
-            VALUE19 = []; MFLAG19 = []; QFLAG19 = []; SFLAG19 = [];
-            VALUE20 = []; MFLAG20 = []; QFLAG20 = []; SFLAG20 = [];
-            VALUE21 = []; MFLAG21 = []; QFLAG21 = []; SFLAG21 = [];
-            VALUE22 = []; MFLAG22 = []; QFLAG22 = []; SFLAG22 = [];
-            VALUE23 = []; MFLAG23 = []; QFLAG23 = []; SFLAG23 = [];
-            VALUE24 = []; MFLAG24 = []; QFLAG24 = []; SFLAG24 = [];
-            VALUE25 = []; MFLAG25 = []; QFLAG25 = []; SFLAG25 = [];
-            VALUE26 = []; MFLAG26 = []; QFLAG26 = []; SFLAG26 = [];
-            VALUE27 = []; MFLAG27 = []; QFLAG27 = []; SFLAG27 = [];
-            VALUE28 = []; MFLAG28 = []; QFLAG28 = []; SFLAG28 = [];
-            VALUE29 = []; MFLAG29 = []; QFLAG29 = []; SFLAG29 = [];
-            VALUE30 = []; MFLAG30 = []; QFLAG30 = []; SFLAG30 = [];
-            VALUE31 = []; MFLAG31 = []; QFLAG31 = []; SFLAG31 = [];
+            numberedList = self.initialize_numbered_1_31_VALUE_MFLAG_QFLAG_SFLAG_lists()
             with open ("./dly_data_as_txt/" + fileId + ".txt", "r") as file:
                 for line in file:
                     ID.append(line[0:11])
                     YEAR.append(line[11:15])
                     MONTH.append(line[15:17])
                     ELEMENT.append(line[17:21])
-                    VALUE1.append(line[21:26]); MFLAG1.append(line[26:27]); QFLAG1.append(line[27:28]); SFLAG1.append(line[28:29])
-                    VALUE2.append(line[29:34]); MFLAG2.append(line[34:35]); QFLAG2.append(line[35:36]); SFLAG2.append(line[36:37])
-                    VALUE3.append(line[37:42]); MFLAG3.append(line[42:43]); QFLAG3.append(line[43:44]); SFLAG3.append(line[44:45])
-                    VALUE4.append(line[45:50]); MFLAG4.append(line[50:51]); QFLAG4.append(line[51:52]); SFLAG4.append(line[52:53])
-                    VALUE5.append(line[53:58]); MFLAG5.append(line[58:59]); QFLAG5.append(line[59:60]); SFLAG5.append(line[60:61])
-                    VALUE6.append(line[61:66]); MFLAG6.append(line[66:67]); QFLAG6.append(line[67:68]); SFLAG6.append(line[68:69])
-                    VALUE7.append(line[69:74]); MFLAG7.append(line[74:75]); QFLAG7.append(line[75:76]); SFLAG7.append(line[76:77])
-                    VALUE8.append(line[77:82]); MFLAG8.append(line[82:83]); QFLAG8.append(line[83:84]); SFLAG8.append(line[84:85])
-                    VALUE9.append(line[85:90]); MFLAG9.append(line[90:91]); QFLAG9.append(line[91:92]); SFLAG9.append(line[92:93])
-                    VALUE10.append(line[93:98]); MFLAG10.append(line[98:99]); QFLAG10.append(line[99:100]); SFLAG10.append(line[100:101])
-                    VALUE11.append(line[101:106]); MFLAG11.append(line[106:107]); QFLAG11.append(line[107:108]); SFLAG11.append(line[108:109])
-                    VALUE12.append(line[109:114]); MFLAG12.append(line[114:115]); QFLAG12.append(line[115:116]); SFLAG12.append(line[116:117])
-                    VALUE13.append(line[117:122]); MFLAG13.append(line[122:123]); QFLAG13.append(line[123:124]); SFLAG13.append(line[124:125])
-                    VALUE14.append(line[125:130]); MFLAG14.append(line[130:131]); QFLAG14.append(line[131:132]); SFLAG14.append(line[132:133])
-                    VALUE15.append(line[133:138]); MFLAG15.append(line[138:139]); QFLAG15.append(line[139:140]); SFLAG15.append(line[140:141])
-                    VALUE16.append(line[141:146]); MFLAG16.append(line[146:147]); QFLAG16.append(line[147:148]); SFLAG16.append(line[148:149])
-                    VALUE17.append(line[149:154]); MFLAG17.append(line[154:155]); QFLAG17.append(line[155:156]); SFLAG17.append(line[156:157])
-                    VALUE18.append(line[157:162]); MFLAG18.append(line[162:163]); QFLAG18.append(line[163:164]); SFLAG18.append(line[164:165])
-                    VALUE19.append(line[165:170]); MFLAG19.append(line[170:171]); QFLAG19.append(line[171:172]); SFLAG19.append(line[172:173])
-                    VALUE20.append(line[173:178]); MFLAG20.append(line[178:179]); QFLAG20.append(line[179:180]); SFLAG20.append(line[180:181])
-                    VALUE21.append(line[181:186]); MFLAG21.append(line[186:187]); QFLAG21.append(line[187:188]); SFLAG21.append(line[188:189])
-                    VALUE22.append(line[189:194]); MFLAG22.append(line[194:195]); QFLAG22.append(line[195:196]); SFLAG22.append(line[196:197])
-                    VALUE23.append(line[197:202]); MFLAG23.append(line[202:203]); QFLAG23.append(line[203:204]); SFLAG23.append(line[204:205])
-                    VALUE24.append(line[205:210]); MFLAG24.append(line[210:211]); QFLAG24.append(line[211:212]); SFLAG24.append(line[212:213])
-                    VALUE25.append(line[213:218]); MFLAG25.append(line[218:219]); QFLAG25.append(line[219:220]); SFLAG25.append(line[220:221])
-                    VALUE26.append(line[221:226]); MFLAG26.append(line[226:227]); QFLAG26.append(line[227:228]); SFLAG26.append(line[228:229])
-                    VALUE27.append(line[229:234]); MFLAG27.append(line[234:235]); QFLAG27.append(line[235:236]); SFLAG27.append(line[236:237])
-                    VALUE28.append(line[237:242]); MFLAG28.append(line[242:243]); QFLAG28.append(line[243:244]); SFLAG28.append(line[244:245])
-                    VALUE29.append(line[245:250]); MFLAG29.append(line[250:251]); QFLAG29.append(line[251:252]); SFLAG29.append(line[252:253])
-                    VALUE30.append(line[253:258]); MFLAG30.append(line[258:259]); QFLAG30.append(line[259:260]); SFLAG30.append(line[260:261])
-                    VALUE31.append(line[261:266]); MFLAG31.append(line[266:267]); QFLAG31.append(line[267:268]); SFLAG31.append(line[268:269]) 
+                    numberedList['VALUE1'].append(line[21:26]); numberedList['MFLAG1'].append(line[26:27]); numberedList['QFLAG1'].append(line[27:28]); numberedList['SFLAG1'].append(line[28:29])
+                    numberedList['VALUE2'].append(line[29:34]); numberedList['MFLAG2'].append(line[34:35]); numberedList['QFLAG2'].append(line[35:36]); numberedList['SFLAG2'].append(line[36:37])
+                    numberedList['VALUE3'].append(line[37:42]); numberedList['MFLAG3'].append(line[42:43]); numberedList['QFLAG3'].append(line[43:44]); numberedList['SFLAG3'].append(line[44:45])
+                    numberedList['VALUE4'].append(line[45:50]); numberedList['MFLAG4'].append(line[50:51]); numberedList['QFLAG4'].append(line[51:52]); numberedList['SFLAG4'].append(line[52:53])
+                    numberedList['VALUE5'].append(line[53:58]); numberedList['MFLAG5'].append(line[58:59]); numberedList['QFLAG5'].append(line[59:60]); numberedList['SFLAG5'].append(line[60:61])
+                    numberedList['VALUE6'].append(line[61:66]); numberedList['MFLAG6'].append(line[66:67]); numberedList['QFLAG6'].append(line[67:68]); numberedList['SFLAG6'].append(line[68:69])
+                    numberedList['VALUE7'].append(line[69:74]); numberedList['MFLAG7'].append(line[74:75]); numberedList['QFLAG7'].append(line[75:76]); numberedList['SFLAG7'].append(line[76:77])
+                    numberedList['VALUE8'].append(line[77:82]); numberedList['MFLAG8'].append(line[82:83]); numberedList['QFLAG8'].append(line[83:84]); numberedList['SFLAG8'].append(line[84:85])
+                    numberedList['VALUE9'].append(line[85:90]); numberedList['MFLAG9'].append(line[90:91]); numberedList['QFLAG9'].append(line[91:92]); numberedList['SFLAG9'].append(line[92:93])
+                    numberedList['VALUE10'].append(line[93:98]); numberedList['MFLAG10'].append(line[98:99]); numberedList['QFLAG10'].append(line[99:100]); numberedList['SFLAG10'].append(line[100:101])
+                    numberedList['VALUE11'].append(line[101:106]); numberedList['MFLAG11'].append(line[106:107]); numberedList['QFLAG11'].append(line[107:108]); numberedList['SFLAG11'].append(line[108:109])
+                    numberedList['VALUE12'].append(line[109:114]); numberedList['MFLAG12'].append(line[114:115]); numberedList['QFLAG12'].append(line[115:116]); numberedList['SFLAG12'].append(line[116:117])
+                    numberedList['VALUE13'].append(line[117:122]); numberedList['MFLAG13'].append(line[122:123]); numberedList['QFLAG13'].append(line[123:124]); numberedList['SFLAG13'].append(line[124:125])
+                    numberedList['VALUE14'].append(line[125:130]); numberedList['MFLAG14'].append(line[130:131]); numberedList['QFLAG14'].append(line[131:132]); numberedList['SFLAG14'].append(line[132:133])
+                    numberedList['VALUE15'].append(line[133:138]); numberedList['MFLAG15'].append(line[138:139]); numberedList['QFLAG15'].append(line[139:140]); numberedList['SFLAG15'].append(line[140:141])
+                    numberedList['VALUE16'].append(line[141:146]); numberedList['MFLAG16'].append(line[146:147]); numberedList['QFLAG16'].append(line[147:148]); numberedList['SFLAG16'].append(line[148:149])
+                    numberedList['VALUE17'].append(line[149:154]); numberedList['MFLAG17'].append(line[154:155]); numberedList['QFLAG17'].append(line[155:156]); numberedList['SFLAG17'].append(line[156:157])
+                    numberedList['VALUE18'].append(line[157:162]); numberedList['MFLAG18'].append(line[162:163]); numberedList['QFLAG18'].append(line[163:164]); numberedList['SFLAG18'].append(line[164:165])
+                    numberedList['VALUE19'].append(line[165:170]); numberedList['MFLAG19'].append(line[170:171]); numberedList['QFLAG19'].append(line[171:172]); numberedList['SFLAG19'].append(line[172:173])
+                    numberedList['VALUE20'].append(line[173:178]); numberedList['MFLAG20'].append(line[178:179]); numberedList['QFLAG20'].append(line[179:180]); numberedList['SFLAG20'].append(line[180:181])
+                    numberedList['VALUE21'].append(line[181:186]); numberedList['MFLAG21'].append(line[186:187]); numberedList['QFLAG21'].append(line[187:188]); numberedList['SFLAG21'].append(line[188:189])
+                    numberedList['VALUE22'].append(line[189:194]); numberedList['MFLAG22'].append(line[194:195]); numberedList['QFLAG22'].append(line[195:196]); numberedList['SFLAG22'].append(line[196:197])
+                    numberedList['VALUE23'].append(line[197:202]); numberedList['MFLAG23'].append(line[202:203]); numberedList['QFLAG23'].append(line[203:204]); numberedList['SFLAG23'].append(line[204:205])
+                    numberedList['VALUE24'].append(line[205:210]); numberedList['MFLAG24'].append(line[210:211]); numberedList['QFLAG24'].append(line[211:212]); numberedList['SFLAG24'].append(line[212:213])
+                    numberedList['VALUE25'].append(line[213:218]); numberedList['MFLAG25'].append(line[218:219]); numberedList['QFLAG25'].append(line[219:220]); numberedList['SFLAG25'].append(line[220:221])
+                    numberedList['VALUE26'].append(line[221:226]); numberedList['MFLAG26'].append(line[226:227]); numberedList['QFLAG26'].append(line[227:228]); numberedList['SFLAG26'].append(line[228:229])
+                    numberedList['VALUE27'].append(line[229:234]); numberedList['MFLAG27'].append(line[234:235]); numberedList['QFLAG27'].append(line[235:236]); numberedList['SFLAG27'].append(line[236:237])
+                    numberedList['VALUE28'].append(line[237:242]); numberedList['MFLAG28'].append(line[242:243]); numberedList['QFLAG28'].append(line[243:244]); numberedList['SFLAG28'].append(line[244:245])
+                    numberedList['VALUE29'].append(line[245:250]); numberedList['MFLAG29'].append(line[250:251]); numberedList['QFLAG29'].append(line[251:252]); numberedList['SFLAG29'].append(line[252:253])
+                    numberedList['VALUE30'].append(line[253:258]); numberedList['MFLAG30'].append(line[258:259]); numberedList['QFLAG30'].append(line[259:260]); numberedList['SFLAG30'].append(line[260:261])
+                    numberedList['VALUE31'].append(line[261:266]); numberedList['MFLAG31'].append(line[266:267]); numberedList['QFLAG31'].append(line[267:268]); numberedList['SFLAG31'].append(line[268:269])
 
             # Create netcdf data object
             with netCDF4.Dataset('./netcdf/ghcn-daily_v3.22.' + datetime.datetime.today().strftime('%Y-%m-%d') + '_' + fileId + '.nc', mode="w", format='NETCDF4') as ds:
@@ -304,19 +273,19 @@ class GHCN:
                 element = ds.createVariable('element', ELEMENT.dtype, ('time',))
                 element[:] = ELEMENT[:]
                 
-                VALUE1 = np.array(VALUE1)
+                VALUE1 = np.array(numberedList['VALUE1'])
                 value1 = ds.createVariable('value1', VALUE1.dtype, ('time',))
                 value1[:] = VALUE1[:]
                 
-                MFLAG1 = np.array(MFLAG1)
+                MFLAG1 = np.array(numberedList['MFLAG1'])
                 mflag1 = ds.createVariable('mflag1', MFLAG1.dtype, ('time',))
                 mflag1[:] = MFLAG1[:]
                 
-                QFLAG1 = np.array(QFLAG1)
+                QFLAG1 = np.array(numberedList['QFLAG1'])
                 qflag1 = ds.createVariable('qflag1', QFLAG1.dtype, ('time',))
                 qflag1[:] = QFLAG1[:]
                 
-                SFLAG1 = np.array(SFLAG1)
+                SFLAG1 = np.array(numberedList['SFLAG1'])
                 sflag1 = ds.createVariable('sflag1', SFLAG1.dtype, ('time',))
                 sflag1[:] = SFLAG1[:]
                 
@@ -335,8 +304,6 @@ class GHCN:
             numberedList['MFLAG' + str(i)] = []
             numberedList['QFLAG' + str(i)] = []
             numberedList['SFLAG' + str(i)] = []
-        #print(numberedLists)  
-        #print(len(numberedLists))
         return numberedList
 
     def run_combined_defs(self, fileId):
@@ -344,7 +311,7 @@ class GHCN:
             self.parse_to_netCDF(fileId)
 
     def go(self):
-            p = Pool(5)
+            p = Pool(3)
             p.map(self, self.get_stationInfo())
 
     def __call__(self, fileId):
