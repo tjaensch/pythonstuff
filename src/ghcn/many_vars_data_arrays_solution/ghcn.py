b9e076e7547256ff8b5dfe71cf81431ad748b71a
Thomas Jaensch <thomas.jaensch@noaa.gov>
2017-08-04 17:15:11 +0000

    Str to S1

--- src/ghcn/many_vars_data_arrays_solution/ghcn.py
+++ src/ghcn/many_vars_data_arrays_solution/ghcn.py
@@ -264,13 +264,13 @@ class GHCN:
                 alt.coverage_content_type = 'coordinate'
                 alt.positive = 'up'
 
-                station_name = ds.createVariable('station_name', 'str')
+                station_name = ds.createVariable('station_name', 'S1')
                 station_name.long_name = self.stationLongNameDict[fileId]
                 station_name.standard_name = 'platform_name'
                 station_name.cf_role = 'timeseries_id'
                 station_name.coverage_content_type = 'coordinate'
 
-                station_id = ds.createVariable('station_id', 'str')
+                station_id = ds.createVariable('station_id', 'S1')
                 station_id.long_name = ID[0]
                 station_id.standard_name = 'platform_id'
 
