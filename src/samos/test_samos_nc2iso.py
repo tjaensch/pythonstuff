e51c7d7f6006e44872a605f7de3a9c255e7a8f53
Thomas Jaensch <tjaensch@pop.nodc.noaa.gov>
2018-03-13 14:39:49 +0000

    PEP8 style and pool count and test to reflect changed file name

--- src/samos/test_samos_nc2iso.py
+++ src/samos/test_samos_nc2iso.py
@@ -4,8 +4,11 @@ import unittest
 from samos_nc2iso import Samos
 
 # Tests
+
+
 class Testsamos(unittest.TestCase):
     """docstring for Testsamos"""
+
     def setUp(self):
         samos = Samos()
         testfile = "/nodc/web/data.nodc/htdocs/coaps/samos/WTEJ/2010/08/WTEJ_20100808v30001.nc"
@@ -18,7 +21,7 @@ class Testsamos(unittest.TestCase):
             os.makedirs("./iso_xml/")
         if not os.path.exists("./final_xml/"):
             os.makedirs("./final_xml/")
-          
+
         # test run defs with one file
         samos.ncdump(testfile)
         samos.add_to_ncml(testfile)
@@ -41,19 +44,21 @@ class Testsamos(unittest.TestCase):
     def test_add_to_ncml(self):
         file = open("./ncml/WTEJ_20100808v30001.ncml", "r")
         data = file.read()
-        self.assertTrue("<title>WTEJ_20100808v30001</title><filesize>146</filesize><path>coaps/samos/WTEJ/2010/08/</path></netcdf>" in data)
+        self.assertTrue(
+            "<title>WTEJ_20100808v30001</title><filesize>146</filesize><path>coaps/samos/WTEJ/2010/08/</path></netcdf>" in data)
 
     def test_xsltproc_to_iso(self):
         file = open("./iso_xml/WTEJ_20100808v30001.xml", "r")
         data = file.read()
-        self.assertTrue("COAPS.SAMOS.WTEJ_20100808v30001" in data)
+        self.assertTrue("gov.noaa.nodc:0156189.WTEJ_20100808v30001" in data)
         # browse graphic generated with bounding box numbers in main XSLT file
         self.assertTrue("<gmd:MD_BrowseGraphic>" in data)
 
     def test_add_collection_metadata(self):
         file = open("./final_xml/WTEJ_20100808v30001.xml", "r")
         data = file.read()
-        self.assertTrue("Global Change Master Directory (GCMD) Data Center Keywords" in data)
+        self.assertTrue(
+            "Global Change Master Directory (GCMD) Data Center Keywords" in data)
 
 # __main__
 if __name__ == '__main__':
