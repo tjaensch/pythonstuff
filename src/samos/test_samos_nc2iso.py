2e3b03d1eb5eead1dad0820ee65456f20981b755
Thomas Jaensch <thomas.jaensch@noaa.gov>
2019-04-12 18:37:53 +0000

    Updated samos and deleted old ghcn and woa13 processes

--- src/samos/test_samos_nc2iso.py
+++ src/samos/test_samos_nc2iso.py
@@ -5,13 +5,12 @@ from samos_nc2iso import Samos
 
 # Tests
 
-
 class Testsamos(unittest.TestCase):
     """docstring for Testsamos"""
 
     def setUp(self):
         samos = Samos()
-        testfile = "/nodc/web/data.nodc/htdocs/coaps/samos/WTEJ/2010/08/WTEJ_20100808v30001.nc"
+        testfile = "/nodc/web/data.nodc/htdocs/coaps/samos/WTEP/2007/04/WTEP_20070415v10001.nc"
         self.ncFiles = samos.find_nc_files()
 
         # Create ncml dir for testing
@@ -28,34 +27,34 @@ class Testsamos(unittest.TestCase):
         samos.xsltproc_to_iso(testfile)
         samos.add_collection_metadata(testfile)
 
-    '''def tearDown(self):
+    def tearDown(self):
         shutil.rmtree("./ncml/")
         shutil.rmtree("./iso_xml/")
         shutil.rmtree("./final_xml")
-        shutil.rmtree("./netcdf3")'''
+        # shutil.rmtree("./netcdf3")
 
     def test_find_nc_files(self):
-        self.assertTrue(len(self.ncFiles) >= 11000)
+        self.assertTrue(len(self.ncFiles) >= 144000)
         self.assertTrue(self.ncFiles)
 
     def test_ncdump(self):
-        self.assertTrue(os.path.isfile("./ncml/WTEJ_20100808v30001.ncml"))
+        self.assertTrue(os.path.isfile("./ncml/WTEP_20070415v10001.ncml"))
 
     def test_add_to_ncml(self):
-        file = open("./ncml/WTEJ_20100808v30001.ncml", "r")
+        file = open("./ncml/WTEP_20070415v10001.ncml", "r")
         data = file.read()
         self.assertTrue(
-            "<title>WTEJ_20100808v30001</title><filesize>146</filesize><path>coaps/samos/WTEJ/2010/08/</path></netcdf>" in data)
+            "<title>WTEP_20070415v10001</title><filesize>127</filesize><path>coaps/samos/WTEP/2007/04/</path></netcdf>" in data)
 
     def test_xsltproc_to_iso(self):
-        file = open("./iso_xml/WTEJ_20100808v30001.xml", "r")
+        file = open("./iso_xml/WTEP_20070415v10001.xml", "r")
         data = file.read()
-        self.assertTrue("gov.noaa.nodc:0156189.WTEJ_20100808v30001" in data)
+        self.assertTrue("COAPS.SAMOS.WTEP_20070415v10001" in data)
         # browse graphic generated with bounding box numbers in main XSLT file
         self.assertTrue("<gmd:MD_BrowseGraphic>" in data)
 
     def test_add_collection_metadata(self):
-        file = open("./final_xml/WTEJ_20100808v30001.xml", "r")
+        file = open("./final_xml/WTEP_20070415v10001.xml", "r")
         data = file.read()
         self.assertTrue(
             "Global Change Master Directory (GCMD) Data Center Keywords" in data)
